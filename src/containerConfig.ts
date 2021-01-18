03df83778d5ca46c19cc4bb163464bca0b85f2a0
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2021-01-18 10:34:39 +0200

    feat: added support for external http readiness (#2)

--- src/containerConfig.ts
+++ src/containerConfig.ts
@@ -1,18 +1,20 @@
 import { readFileSync } from 'fs';
 import { container } from 'tsyringe';
-import config from 'config';
+import config, { IConfig } from 'config';
 import { Probe } from '@map-colonies/mc-probe';
 import { MCLogger, ILoggerConfig, IServiceConfig } from '@map-colonies/mc-logger';
 import { Services } from './common/constants';
+import { getProbe } from './probeInit';
+import { ILogger } from './common/interfaces';
 
 function registerExternalValues(): void {
   const loggerConfig = config.get<ILoggerConfig>('logger');
   const packageContent = readFileSync('./package.json', 'utf8');
   const service = JSON.parse(packageContent) as IServiceConfig;
   const logger = new MCLogger(loggerConfig, service);
-  container.register(Services.CONFIG, { useValue: config });
-  container.register(Services.LOGGER, { useValue: logger });
-  container.register<Probe>(Probe, { useFactory: (container) => new Probe(container.resolve(Services.LOGGER), {}) });
+  container.register<IConfig>(Services.CONFIG, { useValue: config });
+  container.register<ILogger>(Services.LOGGER, { useValue: logger });
+  container.register<Probe>(Probe, { useFactory: getProbe });
 }
 
 export { registerExternalValues };
