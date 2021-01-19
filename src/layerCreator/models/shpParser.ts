d1d4e08fcb327b9c286eb4367343fcb0288bbd59
Shahar s <65118046+cl-shaharshavit-rnd@users.noreply.github.com>
2021-01-19 12:29:33 +0200

    feat: added shape file parser (#3)
    * feat: added shapefile parser

---
+++ src/layerCreator/models/shpParser.ts
@@ -0,0 +1,25 @@
+import { read as readShp } from 'shapefile';
+import { GeoJSON } from 'geojson';
+import { inject } from 'tsyringe';
+import { Services } from '../../common/constants';
+import { ILogger } from '../../common/interfaces';
+
+export class ShpParser {
+  public constructor(@inject(Services.LOGGER) private readonly logger: ILogger) {}
+
+  public async parse(shp: string, dbf: string, encoding = 'utf-8'): Promise<GeoJSON> {
+    const geoJson = await this.toGeoJson(shp, dbf, encoding);
+    return geoJson;
+  }
+
+  private async toGeoJson(shp: string, dbf: string, encoding = 'utf-8'): Promise<GeoJSON> {
+    try {
+      return await readShp(shp, dbf, { encoding: encoding });
+    } catch (err) {
+      const error = err as Error;
+      this.logger.log('error', `failed to parse shapeFile: ${error.message}`);
+      //TODO: add custom error
+      throw err;
+    }
+  }
+}
