1884f2c1662412bd2396c2b8f82d05fd05257006
Shahar s <cl_shaharshavit@rnd-hub.com>
2021-04-18 09:48:50 +0300

    feat: added ivalid shp retry limit

---
+++ tests/mocks/limitingLock.ts
@@ -0,0 +1,11 @@
+import { LimitingLock } from '../../src/watcher/limitingLock';
+
+const acquireMock = jest.fn();
+const isQueueEmptyMock = jest.fn();
+
+const lockMock = ({
+  acquire: acquireMock,
+  isQueueEmpty: isQueueEmptyMock,
+} as unknown) as LimitingLock;
+
+export { lockMock, acquireMock, isQueueEmptyMock };
