5abcf74ed3c368fa01148a7561a19f5acf39e228
Shahar s <cl_shaharshavit@rnd-hub.com>
2021-07-05 11:39:51 +0300

    fix: fixed consts and null mappings

--- src/layerCreator/models/metadataMapper.ts
+++ src/layerCreator/models/metadataMapper.ts
@@ -14,7 +14,7 @@ export class MetadataMapper {
   }
 
   public map(productGeoJson: GeoJSON, metadataGeoJson: GeoJSON, filesGeoJson: GeoJSON, tfwFile: string[]): LayerMetadata {
-    const metadata = ({} as unknown) as LayerMetadata;
+    const metadata = new LayerMetadata();
     this.autoMapModels(metadata, productGeoJson, metadataGeoJson, filesGeoJson, tfwFile);
     this.parseIdentifiers(metadata, metadataGeoJson);
     this.parseSourceDates(metadata, metadataGeoJson);
@@ -95,7 +95,7 @@ export class MetadataMapper {
   }
 
   private castValue(value: unknown, type: string): unknown {
-    if (value === undefined) {
+    if (value === undefined || value === null) {
       return undefined;
     }
     switch (type) {
