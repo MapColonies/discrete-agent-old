bf6b6142d22980ddca4c8c88899ce2ca702d8026
Asaf <asaf.masa@gmail.com>
2021-11-28 13:10:39 +0200

    fix: return 404 response on invalid directory (#84)

--- tests/integration/watchStatus/watchStatus.spec.ts
+++ tests/integration/watchStatus/watchStatus.spec.ts
@@ -5,6 +5,7 @@ import { registerTestValues } from '../testContainerConfig';
 import { getWatchStatusMock, setWatchStatusMock } from '../../mocks/clients/agentDbClient';
 import { Watcher } from '../../../src/watcher/watcher';
 import { registerDefaultConfig } from '../../mocks/config';
+import { directoryExistsMock } from '../../mocks/filesManager';
 import * as requestSender from './helpers/requestSender';
 
 interface StatusResponse {
@@ -32,6 +33,7 @@ describe('watchStatus', function () {
       watcherStatus.watching = true;
     });
     registerDefaultConfig();
+    directoryExistsMock.mockReturnValue(true);
   });
 
   afterEach(function () {
--- tests/mocks/filesManager.ts
+++ tests/mocks/filesManager.ts
@@ -3,13 +3,15 @@ import { opendirMock } from './fs/opendir';
 
 const readAllLinesMock = jest.fn();
 const fileExistsMock = jest.fn();
+const directoryExistsMock = jest.fn();
 const readAsStringSyncMock = jest.fn();
 const openDirMock = opendirMock;
 
 const filesManagerMock = {
   fileExists: fileExistsMock,
+  directoryExists: directoryExistsMock,
   readAllLines: readAllLinesMock,
   readAsStringSync: readAsStringSyncMock,
   openDir: openDirMock,
 } as unknown as FilesManager;
-export { fileExistsMock, readAllLinesMock, readAsStringSyncMock, openDirMock, filesManagerMock };
+export { fileExistsMock, directoryExistsMock, readAllLinesMock, readAsStringSyncMock, openDirMock, filesManagerMock };
